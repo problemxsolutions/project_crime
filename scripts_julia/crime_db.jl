90cc6ddf395018e411fde360374537ea25441dcd
problemxsolutions <23140364+problemxsolutions@users.noreply.github.com>
2021-01-30 14:39:20 -0800

    updated CSV package implementation

--- scripts_julia/crime_db.jl
+++ scripts_julia/crime_db.jl
@@ -1,5 +1,6 @@
 # System: Linux 5.4.0-40-generic, Ubuntu 20.04
 # Julia: Version 1.5.3 (2020-11-09)
+
 # For the full tutorial, please reference URL:
 # https://problemxsolutions.com/project/crime/criminal-analysis-data-storage/
 
@@ -48,7 +49,8 @@
 # the final command brings you back to your normal shell.
 
 # ***************************************************************************
-using CSVFiles
+using CSV
+# using CSVFiles
 using DataFrames # library(tidyverse)
 using Tables
 import Chain # library(magrittr)
@@ -65,11 +67,10 @@ include("$base_dir/project_crime/scripts_julia/project_functions_db.jl")
 # lets write some data to a new table in our db
 base_dir_wx = "$base_dir/data/weather"
 filename = "$base_dir_wx/full_wx_data_2009_2019.csv"
-wx_data_to_db = load(filename) |> DataFrame
+wx_data_to_db = CSV.read(filename, DataFrame)
 
-tabel_name = "weather_julia_load"
+tabel_name = "weather_jl"
 exe_s = string(
-  "CREATE TABLE ", tabel_name, " (",
   "date       timestamp,",
   "datatype   varchar(4),",
   "station    varchar(17),",
@@ -77,11 +78,13 @@ exe_s = string(
   "fl_m       varchar(2),",
   "fl_q       varchar(10),",
   "fl_so      varchar(1),",
-  "fl_t       int,",
-  "units      varchar(25)",
-  ");")
+  "fl_t       varchar(10),",
+  "units      varchar(25)"
+  )
 
-result = LibPQ.execute(pg_connect(), exe_s)
+dbCreateTable(conn = pg_connect(),
+              tableName = tabel_name,
+              column_defs = exe_s)
 
 dbWriteTable_df(conn = pg_connect(),
                 df = wx_data_to_db,
@@ -96,18 +99,36 @@ dbWriteTable_df(conn = pg_connect(),
 
 # Load the rest of the weather, temperature, moon phase and sun time data
 # NOAA CDO temperature data for DCA airport 2009-2019
-base_dir_wx = "$base_dir/data/weather"
+
 filename = "$base_dir_wx/weather_temp_table.csv"
-temperature_data = load(filename) |> DataFrame
-# *** Still need to create table processes ***
+# on error statement
+# temperature_data = load(filename) |> DataFrame
+temperature_data = CSV.read(filename, DataFrame)
+
+tabel_name = "temperature_jl"
+exe_s = string(
+  "date       timestamp, ",
+  "datatype   varchar(4), ",
+  "station    varchar(17), ",
+  "value      int, ",
+  "fl_m       varchar(2), ",
+  "fl_q       varchar(10), ",
+  "fl_so      varchar(1), ",
+  "fl_t       varchar(10), ",
+  "units      varchar(25)"
+  )
+
+dbCreateTable(conn = pg_connect(),
+              tableName = tabel_name,
+              column_defs = exe_s)
 
 dbWriteTable_df(conn = pg_connect(),
                 df = temperature_data,
-                tabel_name = "temperature_jl")
+                tabel_name = tabel_name)
 
 # Moon Phase data from 2009-01-01 to 2020-11-24
 filename = "$base_dir_wx/moon_phase_data.csv"
-moon_phase_data = load(filename) |> DataFrame
+moon_phase_data = CSV.read(filename, DataFrame)
 # *** Still need to create table processes ***
 
 dbWriteTable_df(conn = pg_connect(),
@@ -117,7 +138,7 @@ dbWriteTable_df(conn = pg_connect(),
 
 # Sunrise/Sunset data for Washington DC from 2009-01-01 to 2020-11-24
 filename = "$base_dir_wx/suntimes_data.csv"
-suntimes_data = load(filename) |> DataFrame
+suntimes_data = CSV.read(filename, DataFrame)
 # *** Still need to create table processes ***
 
 dbWriteTable_df(conn = pg_connect(),
@@ -132,7 +153,7 @@ dbWriteTable_df(conn = pg_connect(),
 # DC by Ward Employment Data
 base_dir_emp = "$base_dir/data/employment"
 filename = "$base_dir_emp/PROCESSED_CY2009_2019_unemployment_ward_month_year.csv"
-employment = load(filename) |> DataFrame
+employment = CSV.read(filename, DataFrame)
 # *** Still need to create table processes ***
 
 dbWriteTable_df(conn = pg_connect(),
@@ -141,7 +162,7 @@ dbWriteTable_df(conn = pg_connect(),
 
 # DC Unemployed Insurance Claims
 filename = "$base_dir_emp/PROCESSED_CY2009_2021_weekly_unemployment_insurance_claims.csv"
-doleta_data = load(filename) |> DataFrame
+doleta_data = CSV.read(filename, DataFrame)
 # *** Still need to create table processes ***
 
 dbWriteTable_df(conn = pg_connect(),
@@ -150,7 +171,7 @@ dbWriteTable_df(conn = pg_connect(),
 
 # National Unemployment Rate
 filename = "$base_dir_emp/PROCESSED_USBLS_unemploymentrates_bymonth_2009_2020.csv"
-bls_data = load(filename) |> DataFrame
+bls_data = CSV.read(filename, DataFrame)
 # *** Still need to create table processes ***
 
 dbWriteTable_df(conn = pg_connect(),
@@ -162,7 +183,7 @@ dbWriteTable_df(conn = pg_connect(),
 # Now I will read in the crime data files and write them to a single table in my PostgreSQL database
 base_dir_crime = "$base_dir/data/crime"
 filename = "$base_dir_crime/crime_table_CY2009.csv"
-db_crime_data = load(filename) |> DataFrame
+db_crime_data = CSV.read(filename, DataFrame)
 # Since there I have not created nor defined the crime table previously
 # I will use the first file of data to create the table.  The for-loop
 # below will append the rest of the data to the crime table
@@ -177,7 +198,7 @@ for(source_year in 2010:2020){
   print(paste( "start:", source_year))
 
   filename = string(base_dir_crime,"/", "crime_table_CY", source_year,".csv")
-  db_crime_data = load(filename) |> DataFrame
+  db_crime_data = CSV.read(filename, DataFrame)
 """
 DEV NOTE: Need to determine if my current function appends to the table it
 is writing to if it already has data in it
--- scripts_julia/project_functions_db.jl
+++ scripts_julia/project_functions_db.jl
@@ -2,7 +2,7 @@
 # Julia: Version 1.5.3 (2020-11-09)
 
 # This script provides functions to support my database operations for Project_Crime
-
+import CSV
 using DataFrames # library(tidyverse)
 import Chain # library(magrittr)
 import DBInterface # library(DBI)
@@ -53,7 +53,7 @@ end
   User will need to create table first.
 
   # Arguments
-  - connection: LibPQ Connection to a PostgreSQL database.
+  - conn: LibPQ Connection to a PostgreSQL database.
   Default value is `pg_connect()`, which is defined in a separate function
   - df: DataFrame to upload into database
   - tableName: Name given to db table for the data
@@ -84,109 +84,139 @@ function dbWriteTable_df(;conn=pg_connect(),
 
 end
 
-# ************** CURRENTLY IN DEVELOPMENT **************
-# """
-#   csv_to_db()
-#   Read in CSV file and load it into desired database.
-#   Specify arguments if deviating from defaults.
-#
-#   # Arguments
-#   - file: CSV Filename to read and load into database
-#   - tableName: Name given to db table for the data
-#   - connection: LibPQ Connection to a PostgreSQL database.
-#   Default value is `pg_connect()`, which is defined in a separate function
-# """
-# function csv_to_db(;file,
-#                    tableName="test_table",
-#                    connection=pg_connect())
-#
-#   csv_data = CSVFiles.load(file) |> DataFrame
-#   dbWriteTable_df(conn = connection,
-#                   df = csv_data,
-#                   tabel_name = tableName)
-#
-#   println("Data Loading Complete.")
-#   println() # Get Table Summary (tableName : row count, column count)
-# end
-#
-#
-# """
-#   bulk_table_connections()
-#   Connect to desired database tables to get exploratory information.
-#   Specify arguments if deviating from defaults.
-#
-#   # Arguments
-#   - connection: LibPQ Connection to a PostgreSQL database.
-#   Default value is `pg_connect()`, which is defined in a separate function
-#   - db_tables: Vector. Vector of strings that correspond to the name
-#   values of tables in the connecting database
-#
-#   @return A list that contains a 2 sub-lists.  The first list contains the column names
-#   associated with each table provided by the `db_tables` parameter connection.  The
-#   second is a list of tables via connection, which is not an extract of all data in
-#   each table, but a virtual connection.
-# """
-# function bulk_table_connections(;db_tables, connection=pg_connect())
-#   table_col_names_list = []
-#   db_data_list = []
-#   # for i in 1:length(db_tables)
-#   #   table_col_names_list[i] = LibPQ.column_names(jl_conn = connection,
-#   #                                             name = db_tables[i])
-#   #   db_data_list[i] = tbl(pg_connect(), db_tables[i])
-#   # end
-#   #
-#   # names(table_col_names_list) = db_tables
-#   # names(db_data_list) = db_tables
-#   #
-#   return
-#     Dict(column_names = table_col_names_list,
-#          tables = db_data_list)
-#
-# end
-#
-#
-# """
-#   data_exploration_spatial_initial()
-#   Connect to desired database tables to get exploratory information.
-#   Process spatial tables and output a simple features object
-#
-#   # Arguments
-#   - connection: DBIConnection. DBI connection to a database.
-#   Default value is `pg_connect()`, which is defined in a separate function
-#   - table_name: String. String value of desired table name in the
-#   connecting database
-#   - virtual_data: Table Connection Object. Contains the virtual connection
-#   to the specified table in the database
-#
-#   @return printlns some preliminary exploratory views of the desired table and
-#   returns a simple feature object of the desired data.
-# """
-# function data_exploration_spatial_initial(;
-#   connection = pg_connect(),
-#   table_name,
-#   virtual_data)
-#
-#     # println(virtual_data)
-#     # virtual_data %>% view
-#
-#     # tmp_rc =
-#     #   virtual_data %>%
-#     #   summarise(count = n()) %>%
-#     #   pull()
-#     #
-#     # println(string("Total Rows: ", tmp_rc))
-#     #
-#     # # Display the Table information from the database side
-#     # table_info_output = rpostgis::dbTableInfo(conn = connection,
-#     #                                            name = table_name)
-#     # println(table_info_output)
-#     #
-#     # # Get the geometry data from the database and
-#     # # convert into sf object
-#     spatial_data = []
-#     #   rpostgis::pgGetGeom(conn = connection,
-#     #                       name = table_name,
-#     #                       geom = "geometry") %>%
-#     #   st_as_sf()
-#     return spatial_data
-# end
+
+"""
+  dbCreateTable()
+  Create Tables with string values for table name and column definitions.
+  Please reference PostgreSQL datatypes documentation for more details
+  on datatypes. https://www.postgresql.org/docs/13/datatype.html
+
+  # Arguments
+  - conn: LibPQ Connection to a PostgreSQL database.
+  Default value is `pg_connect()`, which is defined in a separate function
+  - df: DataFrame to upload into database
+  - tableName: Name given to db table for the data
+  - column_defs: string with column names and datatypes.
+    Ex: colname1  datatype1, colname2  datatype2, ...
+"""
+function dbCreateTable(;conn=pg_connect(), tableName, column_defs)
+
+  exe_s = string(
+    "CREATE TABLE ", tableName,
+    " (",column_defs, ");")
+
+  result = LibPQ.execute(conn, exe_s)
+
+end
+"""
+******************************************************
+************** CURRENTLY IN DEVELOPMENT **************
+******************************************************
+"""
+
+"""
+  csv_to_db()
+  Read in CSV file and load it into desired database.
+  Specify arguments if deviating from defaults.
+
+  # Arguments
+  - file: CSV Filename to read and load into database
+  - tableName: Name given to db table for the data
+  - connection: LibPQ Connection to a PostgreSQL database.
+  Default value is `pg_connect()`, which is defined in a separate function
+"""
+function csv_to_db(;file,
+                   tableName="test_table",
+                   connection=pg_connect())
+
+  # csv_data = CSV.read(file, DataFrame)
+  #
+  # dbWriteTable_df(conn = connection,
+  #                 df = csv_data,
+  #                 tabel_name = tableName)
+  #
+  # println("Data Loading Complete.")
+  # println() # Get Table Summary (tableName : row count, column count)
+end
+
+
+"""
+  bulk_table_connections()
+  Connect to desired database tables to get exploratory information.
+  Specify arguments if deviating from defaults.
+
+  # Arguments
+  - connection: LibPQ Connection to a PostgreSQL database.
+  Default value is `pg_connect()`, which is defined in a separate function
+  - db_tables: Vector. Vector of strings that correspond to the name
+  values of tables in the connecting database
+
+  @return A list that contains a 2 sub-lists.  The first list contains the column names
+  associated with each table provided by the `db_tables` parameter connection.  The
+  second is a list of tables via connection, which is not an extract of all data in
+  each table, but a virtual connection.
+"""
+function bulk_table_connections(;db_tables, connection=pg_connect())
+  table_col_names_list = []
+  db_data_list = []
+  # for i in 1:length(db_tables)
+  #   table_col_names_list[i] = LibPQ.column_names(jl_conn = connection,
+  #                                             name = db_tables[i])
+  #   db_data_list[i] = tbl(pg_connect(), db_tables[i])
+  # end
+  #
+  # names(table_col_names_list) = db_tables
+  # names(db_data_list) = db_tables
+  #
+  return
+    Dict(column_names = table_col_names_list,
+         tables = db_data_list)
+
+end
+
+
+"""
+  data_exploration_spatial_initial()
+  Connect to desired database tables to get exploratory information.
+  Process spatial tables and output a simple features object
+
+  # Arguments
+  - connection: DBIConnection. DBI connection to a database.
+  Default value is `pg_connect()`, which is defined in a separate function
+  - table_name: String. String value of desired table name in the
+  connecting database
+  - virtual_data: Table Connection Object. Contains the virtual connection
+  to the specified table in the database
+
+  @return printlns some preliminary exploratory views of the desired table and
+  returns a simple feature object of the desired data.
+"""
+function data_exploration_spatial_initial(;
+  connection = pg_connect(),
+  table_name,
+  virtual_data)
+
+    # println(virtual_data)
+    # virtual_data %>% view
+
+    # tmp_rc =
+    #   virtual_data %>%
+    #   summarise(count = n()) %>%
+    #   pull()
+    #
+    # println(string("Total Rows: ", tmp_rc))
+    #
+    # # Display the Table information from the database side
+    # table_info_output = rpostgis::dbTableInfo(conn = connection,
+    #                                            name = table_name)
+    # println(table_info_output)
+    #
+    # # Get the geometry data from the database and
+    # # convert into sf object
+    spatial_data = []
+    #   rpostgis::pgGetGeom(conn = connection,
+    #                       name = table_name,
+    #                       geom = "geometry") %>%
+    #   st_as_sf()
+    return spatial_data
+end
