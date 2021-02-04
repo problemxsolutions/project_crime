522d67cbee36b957095061fc02985cc154c215e8
problemxsolutions <23140364+problemxsolutions@users.noreply.github.com>
2021-02-03 16:56:59 -0800

    several updates.  Single process script.  This is a conversion from the original R version.

--- scripts_julia/crime_download_api.jl
+++ scripts_julia/crime_download_api.jl
@@ -1,8 +1,8 @@
 # System: Linux 5.4.0-40-generic, Ubuntu 20.04
 # Julia: Version 1.5.3 (2020-11-09)
 
-# For the full tutorial, please reference URL:
-# https://problemxsolutions.com/project/crime/criminal-analysis-planning/
+# For the full tutorial writen for R, please reference URL:
+# https://problemxsolutions.com/project/crime/criminal-analysis-data-search-part-0/
 
 # These are the resources for Washington DC Criminal Data
 # URL: https://opendata.dc.gov/search?collection=Dataset&q=Crime
@@ -11,7 +11,8 @@ using DataFrames # library(tidyverse)
 using HTTP
 using JSON # library(jsonlite)
 using GeoJSON # library(geojsonR)
-import CSVFiles
+import CSV
+
 base_dir = "/home/linux/ProblemXSolutions.com/DataProjects/DC_Crime"
 dir_destination = "$base_dir/data/crime"
 
@@ -20,10 +21,11 @@ if(!ispath(dir_destination))
 
 # Assess the URLs and reduce to unique portions
 
+"""
 # ****************************************************************
-# JSON file
+#                           JSON file
 # ****************************************************************
-base_url = "https://maps2.dcgis.dc.gov/dcgis/rest/services/FEEDS/MPD/MapServer"
+"""base_url = "https://maps2.dcgis.dc.gov/dcgis/rest/services/FEEDS/MPD/MapServer"
 query_end_url = "query?where=1%3D1&outFields=*&outSR=4326&f=json"
 
 crime_data_json = Dict(
@@ -52,12 +54,14 @@ data0 = JSON.Parser.parse(String(doc))
 # data0["fields"]
 
 
+"""
 # ****************************************************************
-# GeoJSON file
+#                       GeoJSON file
 # ****************************************************************
+"""
 base_url = "https://opendata.arcgis.com/datasets/"
 
-crime_data_geojson = Dict(
+crime_data_geojson_dict = Dict(
   "2009" => "73cd2f2858714cd1a7e2859f8e6e4de4_33.geojson",
   "2010" => "fdacfbdda7654e06a161352247d3a2f0_34.geojson",
   "2011" => "9d5485ffae914c5f97047a7dd86e115b_35.geojson",
@@ -71,6 +75,14 @@ crime_data_geojson = Dict(
   "2019" => "f08294e5286141c293e9202fcd3e8b57_1.geojson",
   "2020" => "f516e0dd7b614b088ad781b0c4002331_2.geojson"
 )
+
+# We can access the values using the key
+crime_data_geojson_dict["2014"]
+
+# To access either the key or the value simply reference either then the index number for the element.
+crime_data_geojson_dict.keys[1] # Key
+crime_data_geojson_dict.vals[1] # Value
+
 
 # Alternatively using a named tuple structure
 crime_data_geojson_tup = (
@@ -87,7 +99,7 @@ crime_data_geojson_tup = (
   "2019" => "f08294e5286141c293e9202fcd3e8b57_1.geojson",
   "2020" => "f516e0dd7b614b088ad781b0c4002331_2.geojson"
 )
-# We can access the first element with
+# We can access the first element of the tuple with:
 crime_data_geojson_tup[1]
 
 # To access either the key or the value simply reference 1 or 2
@@ -100,8 +112,9 @@ tmp_url = "https://opendata.arcgis.com/datasets/73cd2f2858714cd1a7e2859f8e6e4de4
 # doc = HTTP.get(tmp_url).body |> String
 # data = GeoJSON.read(doc)
 
-download(tmp_url, "$dir_destination/temp.geojson")
-data = GeoJSON.read(read("$dir_destination/temp.geojson"))
+tmp_file = crime_data_geojson_tup[1][2]
+download(tmp_url, "$dir_destination/$tmp_file")
+data = GeoJSON.read(read("$dir_destination/$tmp_file"))
 data_dict = geo2dict(data)
 
 println(length(data.features))
@@ -109,114 +122,66 @@ println(length(data.features))
 #  # geometry → GeoInterface.Point
 #  # properties → Dict{String, Any}
 
-# # Convert GeoJSON to Dictionary
-# # Slow and single processor
-# data_dict = geo2dict(data)
-# df1 = DataFrame(data_dict["features"][1]["properties"]);
-# for i in data_dict["features"]
-#   df2 = DataFrame(i["properties"]);
-#   df1 = vcat(df1, df2)
-# end
+"""
+Since I want to convert the GeoJSON structure into a DataFrame (tabular structure), I created a function to handle the processes.  This allows me functionalize the process.
 
-# In this next part I'll demonstrate distributing the processing
-using Distributed
-addprocs(length(Sys.cpu_info()))
-# I have 4 processors
-
-@everywhere using DataFrames
-function bind_rows_distributed()
-    nheads = @distributed (vcat) for i in data_dict["features"]
-      df_i = DataFrame(i["properties"]);
-    end
-end
-
-@time begin
-  df_combined = bind_rows_distributed()
-end
-# julia> 33.885074 seconds (6.69 M allocations: 227.335 MiB, 2.10% gc time)
-# julia> 31.356703 seconds (6.50 M allocations: 217.471 MiB, 0.55% gc time)
-# Much faster than the original for loop
-
+In the function I take the GeoJSON data, convert each of the elements in the document to a DataFrame object.  Then bind all the DataFrames into one DataFrame.  The final [`something.(data, "")`]https://docs.julialang.org/en/v1/base/base/#Base.something) broadcasted function takes the data and changes `nothing` values into the second argument value.  In my case, I used the `""` to make it a blank value.
+"""
 function bind_rows(df)
     df_v = [DataFrame(i["properties"]) for i in df]
     df_base = df_v[1]
     for i in df_v[2:end]
       allowmissing!(append!(df_base, i, cols=:union))
     end
-    df_base
+    df_base = something.(df_base, "")
+    return df_base
 end
 
-@time begin
-  df_combined_nodist = bind_rows(data_dict["features"])
-end
-# julia> 5.648471 seconds (8.50 M allocations: 523.706 MiB, 4.96% gc time)
+"""
+## Functionalize the Process
+Here, we combine processes described above into a function.  To add flexibility I have included an argument to allow the processing of local files over getting the data directly from the API each time.
 
-# this tells us the results where the same
-df_combined_nodist == df_combined
+You can see the structure is independent and can be distributed.  There is no need for MapReduce.  Nothing to collect.  Just perform a task.
 
-# As we can see the the process didnt save time
-
-# Remove the data we no longer need.
-# This is valid if you use the GeoJSON method as well.
-data0 = nothing
-query_end_url = nothing
-crime_data_json = nothing
-df_combined = nothing
-data_dict = nothing
-doc = nothing
-
-CSVFiles.save("$dir_destination/CY2009_DC_CRIME_jl.csv", df_combined_nodist)
-
-# NO METHOD TO SAVE GeoJSON file in the package yet.
-# GeoJSON.save("$dir_destination/2009_DC_CRIME_jl.geojson", data)
-
-
-# ****************************************************************
-function process_geojson_file(x1, x2, url, dir)
-  # println(string("Processing ", x1))
+"""
+function process_geojson_file(;src_yr, src_url, url, dir, local_files=false)
+  println("Starting $src_yr")
   # Get Data
-  cu = string(url, x2);
-  doc = HTTP.get(cu).body |> String
+  if !local_files
+    cu = string(url, src_url);
+    doc = HTTP.get(cu).body |> String
+  else
+    doc = string("$dir/$src_url")|> read |> String
+  end
+
   data = GeoJSON.read(doc)
-  data_dict = geo2dict(data)
+  data_dict = GeoJSON.geo2dict(data)
 
   df = bind_rows(data_dict["features"])
-  CSVFiles.save(string("$dir/crime_table_CY", x1,"_jl.csv"), df)
-  println(string(x1, " completed!"))
-  println("Processing Complete")
+  df = something.(df, "")
+  # CSVFiles.save(string("$dir/crime_table_CY", x1,"_jl.csv"), df)
+  CSV.write(
+    string("$dir/crime_table_CY", src_yr,"_jl.csv"),
+    df,
+    missingstring = ""
+  )
+  println("$src_yr completed!")
 end
 
-# This is a single processor.  It took a long time to complete.
-# From my observation
-for i in crime_data_geojson
+"""
+# Execute the process.
+# For information on the distributed version of the process check out the distributed_etl_crime.jl script.
+
+# For the full tutorial, please reference URL:
+# URL: https://problemxsolutions.com/distributed-computing/distributed-computing-with-julia-by-example/
+"""
+for i in crime_data_geojson_tup
   src_yr = i[1]
   src_url = i[2]
   process_geojson_file(
-    src_yr,
-    src_url,
-    base_url,
-    dir_destination)
+    src_yr = src_yr,
+    src_url = src_url,
+    url = url,
+    dir = dir,
+    local_files = local_d)
 end
-
-
-# ***** NOT WORKING YET, IN DEVELOPMENT ***** #
-"""
-# Now we will get all the data and store in local directory.
-# We could just as easily load directly into a database if it were configured already.
-# That will be discussed in a later script.
-
-@everywhere using CSVFiles, DataFrames, GeoJSON, HTTP
-function get_all_geojson_files(x, url, dir)
-    nheads = @distributed for i in x
-      src_yr = i[1];
-      src_url = i[2];
-      process_geojson_file(src_yr, src_url, url, dir)
-
-    end
-end
-
-@time begin
-  get_all_geojson_files(crime_data_geojson, base_url, dir_destination)
-end
-# julia>
-"""
